b3ffaa4c21ab6d2ed29595e9df9c1bfd79638a77
Fernando <fernydiaz62@gmail.com>
2024-03-19 21:49:04 -0300

    update

--- src/app/shared/components/card-home-rooms/card-home-rooms.component.ts
+++ src/app/shared/components/card-home-rooms/card-home-rooms.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe, TitleCasePipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, OnInit, input, signal } from '@angular/core';
 import { roomInformation } from '@interfaces/card-home.interface';
 
 import { NzIconModule } from 'ng-zorro-antd/icon';
@@ -11,6 +11,29 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
   templateUrl: './card-home-rooms.component.html',
   styleUrl: './card-home-rooms.component.scss',
 })
-export class CardHomeRoomsComponent {
+export class CardHomeRoomsComponent implements OnInit {
   public rooms = input.required<roomInformation[]>();
+  public color = signal<string>('');
+
+  ngOnInit(): void {}
+
+  setColor(room: string): string {
+    if (room === 'libre') {
+      return '#4eae0f';
+    }
+
+    if (room === 'limpieza') {
+      return '#e2970b';
+    }
+
+    return '#ae0f0f';
+  }
+
+  setIcon(room: string): string {
+    if (room === 'limpieza') {
+      return 'fa-solid fa-broom';
+    }
+
+    return 'fa-solid fa-bed';
+  }
 }
